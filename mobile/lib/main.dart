9b0bf6c7555e5147c0b9221e23c71572c309090f
Martin gdn <44martin.g@gmail.com>
2020-11-24 15:06:17 +0100

    feat add main page

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,170 +1,186 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
+import 'package:mobile/takeExpense.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async' show Future;
 
-const url = 'https://example.com/whatsit/create';
+void main() {
+  loadAsset();
+  runApp(MyApp());
+}
 
-Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
+Future<String> loadAsset() async {
+  return await rootBundle.loadString('assets/images/logo.png');
+}
 
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ticketor',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-    ),
-  );
-}
-
-// A screen that allows users to take a picture using a given camera.
-class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
-
-  const TakePictureScreen({
-    Key key,
-    @required this.camera,
-  }) : super(key: key);
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<TakePictureScreen> {
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
+      home: MyHomePage(title: 'Ticketor'),
     );
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(title: Text('Note de frais')),
-      // Wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner
-      // until the controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage('images/logo.png'),
+              width: Theme.of(context).iconTheme.size,
+            ),
+            Text(
+              'Note de frais acceptés',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              'Note de frais en cours',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            FlatButton(
+              onPressed: () async {
+                // Ensure that plugin services are initialized so that `availableCameras()`
+                // can be called before `runApp()`
+                WidgetsFlutterBinding.ensureInitialized();
 
-            // Construct the path where the image should be saved using the
-            // pattern package.
-            final path = join(
-              // Store the picture in the temp directory.
-              // Find the temp directory using the `path_provider` plugin.
-              (await getTemporaryDirectory()).path,
-              '${DateTime.now()}.png',
-            );
+                // Obtain a list of the available cameras on the device.
+                final cameras = await availableCameras();
 
-            // Attempt to take a picture and log where it's been saved.
-            await _controller.takePicture(path);
+                // Get a specific camera from the list of available cameras.
+                final firstCamera = cameras.first;
 
-            // If the picture was taken, display it on a new screen.
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(imagePath: path),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        TakeExpenseScreen(camera: firstCamera),
+                  ),
+                );
+              },
+              child: Text(
+                'Envoyer une note de frais',
+                style: TextStyle(
+                    fontFamily:
+                        Theme.of(context).textTheme.headline6.fontFamily,
+                    fontStyle: Theme.of(context).textTheme.headline6.fontStyle,
+                    fontSize: Theme.of(context).textTheme.headline6.fontSize,
+                    color: Colors.white),
               ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
+              color: Colors.orange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              ),
+
+              // This trailing comma makes auto-formatting nicer for build methods.
+            ),
+            FlatButton(
+              onPressed: () async {
+                // Ensure that plugin services are initialized so that `availableCameras()`
+                // can be called before `runApp()`
+                WidgetsFlutterBinding.ensureInitialized();
+
+                // Obtain a list of the available cameras on the device.
+                final cameras = await availableCameras();
+
+                // Get a specific camera from the list of available cameras.
+                final firstCamera = cameras.first;
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        TakeExpenseScreen(camera: firstCamera),
+                  ),
+                );
+              },
+              child: Text(
+                'Voir en details',
+                style: TextStyle(
+                    fontFamily:
+                        Theme.of(context).textTheme.headline6.fontFamily,
+                    fontStyle: Theme.of(context).textTheme.headline6.fontStyle,
+                    fontSize: Theme.of(context).textTheme.headline6.fontSize,
+                    color: Colors.white),
+              ),
+              color: Colors.orange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              ),
+
+              // This trailing comma makes auto-formatting nicer for build methods.
+            ),
+          ],
+        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-  }
-}
-
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Validation')),
-        // The image is stored as a file on the device. Use the `Image.file`
-        // constructor with the given path to display the image.
-        body: Image.file(File(imagePath)),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.send),
-            onPressed: () async {
-              debugPrint('movieTitle: $imagePath');
-              File _imageFile = File(imagePath);
-              if (_imageFile != null) {
-                List<int> imageBytes = await _imageFile.readAsBytes();
-                String base64Image = base64Encode(imageBytes);
-
-                var url = 'https://example.com/whatsit/create';
-
-                var response = await http
-                    .post(url, body: {'user': 'doodle', 'image': base64Image});
-                if (response.statusCode != 200) {
-                  Navigator.pop(context);
-                } else {}
-                print('Response status: ${response.statusCode}');
-                print('Response body: ${response.body}');
-
-                print(await http.read('https://example.com/foobar.txt'));
-              }
-            }),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
