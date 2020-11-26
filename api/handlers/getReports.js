4eea8ea58c751262485d436507e96c8d14d6e7ee
Simon PEROLS <simon@perols.dev>
2020-11-26 13:53:37 +0100

    feat: get by specific id

--- api/handlers/getReports.js
+++ api/handlers/getReports.js
@@ -2,18 +2,53 @@ const client = require('../config/odoo.js');
 const model = require('../model.js');
 
 function getReports(request, h) {
-    return new Promise((resolve, reject) => {
-        client.models.methodCall('execute_kw', [
-            client.getDb(), request.auth.credentials.uid, request.auth.credentials.password,
-            "hr.expense", "search_read", [[['employee_id', '=', request.auth.credentials.uid]]]
-        ], function (error, expenses) {
-            if(error) {
-                reject(error);
-            } else {
-                resolve(expenses.map(expense => model.fromOdoo(expense)))
-            }
+    if(request.params.id) {
+        return new Promise((resolve, reject) => {
+            client.models.methodCall('execute_kw', [
+                client.getDb(), request.auth.credentials.uid, request.auth.credentials.password,
+                "hr.expense", "search_read", [[['employee_id', '=', request.auth.credentials.uid], ['id', '=', request.params.id]]]
+            ], function (error, expenses) {
+                if(error) {
+                    reject(error);
+                } else {
+                    if (expenses.length > 0) {
+                        var expense = model.fromOdoo(expenses[0]);
+
+                        if(expenses[0].message_main_attachment_id.length > 0) {
+                            client.models.methodCall('execute_kw', [
+                                client.getDb(), request.auth.credentials.uid, request.auth.credentials.password,
+                                "ir.attachment", "search_read", [[['id', '=', expenses[0].message_main_attachment_id[0]]]]
+                            ], function (error, attachment) {
+                                if (error) {
+                                    reject(error);
+                                } else {
+                                    expense.attachment = attachment[0].datas;
+                                    resolve(expense);
+                                }
+                            });
+                        } else {
+                            resolve(expense);
+                        }
+                    } else {
+                        resolve(null);
+                    }
+                }
+            });
         });
-    });
+    } else {
+        return new Promise((resolve, reject) => {
+            client.models.methodCall('execute_kw', [
+                client.getDb(), request.auth.credentials.uid, request.auth.credentials.password,
+                "hr.expense", "search_read", [[['employee_id', '=', request.auth.credentials.uid]]]
+            ], function (error, expenses) {
+                if(error) {
+                    reject(error);
+                } else {
+                    resolve(expenses.map(expense => model.fromOdoo(expense)))
+                }
+            });
+        });
+    }
 }
 
 module.exports = getReports;
